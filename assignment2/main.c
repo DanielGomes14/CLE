f8da0b829d3553b0a006988798bda066d0a67676
D1scak3 <72574899+D1scak3@users.noreply.github.com>
2022-03-19 16:33:42 +0000

    0's na diagonal
    0's na diagonal impedem o cálculo correto do resultado final

--- assignment2/main.c
+++ assignment2/main.c
@@ -1,84 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define PATH = "/datasets/"
 
-void gaussian_elimination(int order, double matrix[order][order]){
-    //transform normal matrix to equivalent triangular matrix
+int gaussian_elimination(int order, double matrix[order][order]){
+    // applies gaussian elimination to the given matrix
 
-    float c, sum;
-    float res[order];
+    int swap_count = 0;
 
-    // loop for generation of upper triangular matrix
+    // for(int i = 0; i < order; i++){
+    //     for(int j = 0; j < order; j++){
+    //         printf("%f\t", matrix[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // getchar();
+
+    // upper triangular transformation
     for(int i = 0; i < order; i++){
-        for(int j = 0; j < order; j++){
-            if(j > i){
-                c = matrix[i][j] / matrix[i][i];
+        for(int j = i + 1; j < order; j++){
+            if(fabs(matrix[i][i]) < fabs(matrix[j][i])){
+                swap_count++;
                 for(int k = 0; k < order; k++){
-                    matrix[k][j] = (c * matrix[i][k]);
+                    double temp;
+                    temp = matrix[i][k];
+                    matrix[i][k] = matrix[j][k];
+                    matrix[j][k] = temp;
                 }
             }
         }
-    }
 
-    // something???
-    res[order] = matrix[order][order + 1] / matrix[order][order];
-
-    // loop for backward substituition
-    for(int i = order - 1; i >= 1; i--){
-        sum = 0;
-        
-        for(int j = i + 1; j <= order; j++)
-            sum = sum + matrix[i][j] * x[j];
-        
-        res[i] = (matrix[i][order + 1] - sum) / matrix[i][i];
-    }
-
-
-    float A[20][20],x[10],sum=0.0;
-    double c;
-
-    for(int j = 1; j <= order; j++) /* loop for the generation of upper triangular matrix*/
-    {
-        for(int i = 1; i <= order; i++)
-        {
-            if(i > j)
-            {
-                c = *(matrix + j * order + i) / *(matrix + j * order + j * order);
-                for(int k = 1; k <= order + 1; k++)
-                {
-                    matrix[(i* k) - 1] = matrix[(i * k) -1] - (c * matrix[(j * k) -1]);
-                    *(matrix + i * k - 1) = *(matrix + i * k - 1) - (c * (*(matrix + j * order * k)))
-                }
+        // gauss elimination
+        for(int j = 0; j < order; j++){
+            double term = matrix[j][i] / matrix[i][i];
+            if(matrix[i][i] == 0)
+            printf("ZEROOOOOOOOOOOOOOOOOOOOOOOOO\n");
+            for(int k = 0; k < order; k++){
+                matrix[j][k] = matrix[j][k] - (term * matrix[i][k]);
             }
         }
+
+        // for(int i = 0; i < order; i++){
+        //     for(int j = 0; j < order; j++){
+        //         printf("%f\t", matrix[i][j]);
+        //     }
+        //     printf("\n");
+        // }
+        // getchar();
+
     }
 
-    x[n]=A[n][n+1]/A[n][n];
-
-    /* this loop is for backward substitution*/
-    for(int i = order - 1; i >= 1; i--)
-    {
-        sum=0;
-        for(int j = i + 1; j <=n ; j++)
-        {
-            sum=sum+A[i][j]*x[j];
-        }
-        x[i]=(matrix[i][n+1]-sum)/matrix[i][i];
-    }
-
-    printf("\nThe solution is: \n");
-    for(i=1; i<=n; i++)
-    {
-        printf("\nx%d=%f\t",i,x[i]); /* x1, x2, x3 are the required solutions*/
-    }
-
+    return swap_count;
 }
 
-int calcDeterminant(float* matrix){
+int determinant(int order, double matrix[order][order]){
     // calculates determinant of triangular matrix
-    return 0;
+
+    double det = 1;
+    int swap_count = gaussian_elimination(order, matrix);
+
+    for(int i = 0; i < order; i++)
+        det = det * matrix[i][i];
+
+    return det * pow(-1, swap_count);
 }
 
 int main(int argc, char* argv[]) {
@@ -88,7 +74,7 @@ int main(int argc, char* argv[]) {
         exit(0);
     }
     else{
-        printf("Files to read: <%d>", argc-1);
+        printf("Files to read: <%d>\n", argc-1);
     }
 
     for(int i = 1; i < argc; i++){
@@ -109,21 +95,16 @@ int main(int argc, char* argv[]) {
 
         // reads values of matrix
         double matrix[order][order];
-        while(fread(&matrix, sizeof(matrix), 1, file)){
-            printf("\n\n");
-            for(int i = 0; i < order; i++){
-                for(int j = 0; j < order; j++){
-                    printf("%f\t", matrix[i][j]);
-                }
-                printf("\n");
-            }
+        int j = 0;
+
+        printf("---------------------File <%s>---------------------\n", argv[i]);
+        printf("Amount of matrixes: <%d>\tOrder of matrixes: <%d>\n", amount, order);
+        while(fread(&matrix, sizeof(matrix), 1, file)){  // reads matrix from .bin file
+            // calculates determinant, gaussian elimination is applied inside
+            printf("\tMatrix nº: <%d>\tDeterminant: <%d>\n", j + 1, determinant(order, matrix));
+            j++;
         }
-
-        // transform normal matrix in equivalent triangular matrix
-        gaussian_elimination(order, matrix);
-
-        // calculate determinant
-        // int res = calcDeterminant(matrix);
+        printf("\n\n");
 
     }
 
