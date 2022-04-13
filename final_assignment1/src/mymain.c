d543a090cbdaa5b24830ccd2eba7f7ee3a8959bb
Daniel <dagomes@ua.pt>
2022-04-13 12:07:35 +0100

    init struct, not tested

--- final_assignment1/src/mymain.c
+++ final_assignment1/src/mymain.c
@@ -9,6 +9,8 @@
 
 #include "./cmd/processCommandLine.h"
 
+int *statusWorker;
+
 /**
  * @brief Main function
  * 
@@ -22,30 +24,59 @@
  * @param argv array with the words from the command line
  * @return int (status of operation)
  */
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-    int thread_amount = 0;
+    int thread_amount = 0; // number of threads;
+    int *status_p;         // pointer to execution status
 
     // no inputs where given
-    if(argc == 1){
+    if (argc == 1)
+    {
         perror("No arguments were provided.");
         exit(-1);
     }
 
     // processes command line information
-    if(processInput(argc, argv, &thread_amount))
-        exit(-1);    
-    
+    if (processInput(argc, argv, &thread_amount))
+        exit(-1);
 
     printf("THREAD AMOUNT: %d\n", thread_amount);
-    pthread_t t_id[thread_amount];
+    //
+    pthread_t tIdWorker[thread_amount];  //  workers internal thread id array
+    unsigned int workers[thread_amount]; // workers application defined thread id array
+
+    // initialise workers array
+    for (int t_ind = 0; t_ind < thread_amount; t_ind++)
+        workers[t_ind] = t_ind;
+
+    statusWorker = malloc(sizeof(int) * thread_amount);
+
+    for (int t = 0; t < thread_amount; t++)
+    {
+        //create(t)
+        if (pthread_create(&tIdWorker[t], NULL, work, workers[t]) != 0) /* thread producer */
+        {
+            perror("error on creating thread worker");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for (int t = 0; t < thread_amount; t++)
+    {
+        if (pthread_join(tIdWorker[t], (void *)&status_p) != 0) /* thread producer */
+        {
+            perror("error on waiting for thread worker");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("thread worker, with id %d, has terminated: ", t);
+        printf("its status was %d\n", *status_p);
+    }
 
     return 0;
 }
 
-
-
-// main  
+// main
 //     process cmd
 //     create workers in wait mode
 //     for file
@@ -54,3 +85,20 @@ int main(int argc, char *argv[]){
 //     signal workers to end(signal flag to false)
 //     espera por workers
 //     end
+
+/**
+ * @brief Worker Function
+ * 
+ * Does the worker tasks
+ * @param par pointer to application defined worker identification
+ */
+void *work(void * par){
+    // TODO: call worker functions
+
+    unsigned int id = *((unsigned int *) par), // worker id //
+               val;  
+
+    //end work
+    statusWorker[id] = EXIT_SUCCESS;
+    pthread_exit (&statusWorker[id]);
+}
--- final_assignment1/src/shared/shared.c
+++ final_assignment1/src/shared/shared.c
@@ -6,6 +6,16 @@
 extern int vowelCounter = 0;
 extern int consonantCounter = 0;
 extern int wordCount = 0;
+//https://stackoverflow.com/questions/50083744/how-to-create-an-array-without-declaring-the-size-in-c
+
+/** \brief  Worker threads return status array*/
+extern int *statusWorkers;
+
+/** \brief locking flag which warrants mutual exclusion inside the monitor */
+static pthread_mutex_t accessCR = PTHREAD_MUTEX_INITIALIZER;
+
+/** \brief flag which warrants that the data transfer region is initialized exactly once */
+static pthread_once_t init = PTHREAD_ONCE_INIT;
 
 /**
  * @brief Initialise the data transfer region.
